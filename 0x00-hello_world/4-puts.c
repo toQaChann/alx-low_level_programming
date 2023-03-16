d3ead531b8cd6f13bce2f582c393b48937c5a06f
toQachann <tokachan015@gmail.com>
2023-03-16 14:58:01 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
-int main(void){
-        char str[]="Programming is like building a multilingual puzzle\n";
+int main (void)
+{
+        char str[]="\"Programming is like building a multilingual puzzle\n";
 	puts(str);
 	return 0;
 }
