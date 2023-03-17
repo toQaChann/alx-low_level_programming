23b35a1c8c9ff0a58ce0b33f066e2ab1e57734a9
toQachann <tokachan015@gmail.com>
2023-03-16 17:58:01 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
 
-/*
+/**
  * main - Entry point
  *
- * Description : A C program that prints with puts function
+ * Description: A C program that prints with put function
  *
- * Return : Alwasy 0 (Success)
+ * Return: Alwasy 0 (Success)
 */
 
 int main(void)
 {
-	char str[] = "\"Programming is like building a multilingual puzzle\n";
-	puts(str);
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
