ba4932b1e91aef379505fffbe24c27b86aa9c190
toQachann <tokachan015@gmail.com>
2023-03-19 22:13:36 -0700

    nbnfbfnb

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -12,8 +12,7 @@ int main(void)
 {
 	int num;
 
-	num = 0;
-	for (num; num < 10; num++)
+	for (num = 0; num < 10; num++)
 	{
 		printf("%d", num);
 	}
--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -11,9 +11,10 @@
 int main(void)
 {
 	char num;
+	int i;
 
 	num = '0';
-	for (int i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		putchar(num);
 		num = (int)num + 1;
