c586f1bd33cf7e3cd1645bd98ce1386d3a6ac316
toQachann <tokachan015@gmail.com>
2023-03-21 06:42:13 -0700

    hjh

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print putchar
+ *
+ * Return: will return 0
+ *
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
