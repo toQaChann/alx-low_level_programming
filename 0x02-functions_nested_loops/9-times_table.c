63c5995f209853ec156e750102759e16b3307eb0
toQachann <tokachan015@gmail.com>
2023-03-22 02:05:38 -0700

    victory

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ *times_table - check case
+ *
+ *Return: will return noting
+*/
+
+void times_table(void)
+{
+	int r;
+	int n;
+
+	for(int i = 0; i <= 9; i++)
+	{
+		for(int j = 0; j <= 9; j++)
+		{
+			r = i * j;
+			if(j == 0)
+			{
+				_putchar(r +'0');
+				_putchar(',');
+			}
+			else if(j > 0 && r <= 9)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(r+'0');
+				if(j != 9)
+				{
+					_putchar(',');
+				}
+			}
+			else
+			{
+				n = r % 10;
+				r = r / 10;
+				_putchar(' ');
+				_putchar(r+'0');
+				_putchar(n+'0');
+				if(j != 9)
+				{
+					_putchar(',');
+				}
+			}
+		}
+		putchar('\n');
+	}
+}
