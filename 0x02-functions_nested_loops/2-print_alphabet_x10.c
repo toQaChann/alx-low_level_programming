0e0c0e0ae95e1a777c9df6df9869d4950a417a36
toQachann <tokachan015@gmail.com>
2023-03-21 19:55:59 -0700

    kmg

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - function to print alph
+ *
+ *Return: will return nothinq
+*/
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		print_alphabet();
+	}
+	_putchar('\n');
+}
