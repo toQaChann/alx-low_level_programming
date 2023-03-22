a46759ed04b7063c8f6117a13323da0b824bc345
toQachann <tokachan015@gmail.com>
2023-03-21 23:23:24 -0700

    kmj

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *print_sign - check case
+ *@n: the character that will be checked
+ *
+ *Return: will return 1 or 0 or -1
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+        {
+		_putchar('+');
+		_putchar('\n');
+                return (1);
+        }
+        else if (n == 0)
+        {
+		_putchar('0');
+                _putchar('\n');
+                return (0);
+        }
+        else
+        {
+		_putchar('-');
+                _putchar('\n');
+                return (-1);
+        }
+
+}
