48cdc1cddcf7b8a9f1bff26b8dd218d0e72748c5
toQachann <tokachan015@gmail.com>
2023-03-21 23:28:44 -0700

    ji

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,22 +10,19 @@
 int print_sign(int n)
 {
 	if (n > 0)
-        {
+	{
 		_putchar('+');
-		_putchar('\n');
-                return (1);
-        }
-        else if (n == 0)
-        {
+		return (1);
+	}
+	else if (n == 0)
+	{
 		_putchar('0');
-                _putchar('\n');
-                return (0);
-        }
-        else
-        {
+		return (0);
+	}
+	else
+	{
 		_putchar('-');
-                _putchar('\n');
-                return (-1);
-        }
+		return (-1);
+	}
 
 }
