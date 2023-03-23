220ddea440b23085b64ce35b28a0b46c527d3c45
toQachann <tokachan015@gmail.com>
2023-03-23 06:18:29 -0700

    mm

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_isupper - check case
+ *@c: the start number
+ *
+ *Return: will return 0 or 1
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 58)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
