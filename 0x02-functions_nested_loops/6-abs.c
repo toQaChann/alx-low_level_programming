e3cca882dbe08738591c91d7f17479488c47063e
toQachann <tokachan015@gmail.com>
2023-03-22 17:42:55 -0700

    lk

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_abs - check case
+ *@n: the character that will be checked
+ *
+ *Return: will return the value of operation
+*/
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,3 +6,4 @@ int _isalpha(int c);
 int print_sign(int n);
 void times_table(void);
 void print_to_98(int n);
+int _abs(int);
