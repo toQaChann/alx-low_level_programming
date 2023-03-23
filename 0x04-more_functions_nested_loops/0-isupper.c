2cf82488aedeef9315978f6986cd1b0f8a9d7f44
toQachann <tokachan015@gmail.com>
2023-03-23 06:11:46 -0700

    mm

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_isupper - check case
+ *@c: the start number 
+ *
+ *Return: will return 0 or 1
+*/
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 91)
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}
