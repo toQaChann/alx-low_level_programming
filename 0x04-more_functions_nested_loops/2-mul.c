db72111483dd91eb8f539ab3430c313e0474750d
toQachann <tokachan015@gmail.com>
2023-03-23 07:09:55 -0700

    mm

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *mul - check case
+ *@a: the num will be added
+ *@b: the num will be added
+ *
+ *Return: will return the value of operation
+*/
+
+int mul(int a, int b)
+{
+
+}
