b9de9537c1a6d1a7ab0cc6065738a341391bad0f
toQachann <tokachan015@gmail.com>
2023-03-22 17:58:46 -0700

    lk

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *add - check case
+ *@a: the num will be added
+ *@b: the num will be added
+ *
+ *Return: will return the value of operation
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,3 +8,4 @@ void times_table(void);
 void print_to_98(int n);
 int _abs(int);
 int print_last_digit(int n);
+int add(int a, int b);
