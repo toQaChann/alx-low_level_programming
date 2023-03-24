6c7fb7ff445afa168402abed417d355f6ad59e96
toQachann <tokachan015@gmail.com>
2023-03-24 03:07:52 -0700

    mm

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ *main - entry point and usin' header files
+ *
+ *Descriptions: test functions
+ *
+ *Return: will return 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 != 0 || i % 5 != 0)
+		{
+			printf("%d ", i);
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			if (i != 100)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("Buzz\n");
+			}
+		}
+	}
+	return (0);
+}
