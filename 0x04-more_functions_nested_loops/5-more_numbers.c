5e9e1ca7f27175bc23835ccf09956d173b86bec8
toQachann <tokachan015@gmail.com>
2023-03-26 02:41:10 -0700

    m

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,18 +8,19 @@
 
 void more_numbers(void)
 {
-	int i, num;
+	int i, num, p;
 
 	for (i = 1; i <= 10; i++)
 	{
 		for (num = 0; num <= 14; num++)
 		{
+			p = num;
 			if (num > 9)
 			{
 				_putchar('1');
-				num = num % 10;
+				p = num % 10;
 			}
-			_putchar(num + '0');
+			_putchar(p + '0');
 
 		}
 		_putchar('\n');
