dcf7bea3607af336778897adce2648dc5f027660
toQachann <tokachan015@gmail.com>
2023-03-25 22:47:43 -0700

    m

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -18,6 +18,7 @@ void print_triangle(int size)
 		{
 			if (size <= 0)
 			{
+				_putchar('\n');
 				break;
 			}
 			if (j < p)
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,10 +14,14 @@ void more_numbers(void)
 	{
 		for (num = 0; num <= 14; num++)
 		{
-			num = num % 10;
+			if (num > 9)
+			{
+				_putchar('1');
+				num = num % 10;
+			}
+			_putchar(num + '0');
 
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
