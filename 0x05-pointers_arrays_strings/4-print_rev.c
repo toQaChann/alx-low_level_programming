457577654884c36f4b01332496ca073c8f748fdd
toQachann <tokachan015@gmail.com>
2023-03-28 19:13:08 -0700

    m

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,13 +10,13 @@
 void print_rev(char *s)
 {
 	int c, i;
-
-        c = 0;
-        while (s[c] != '\0')
-        {
-                c++;
-        }
-        for (i = c - 1; i >= 0; i--)
+	
+	c = 0;
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	for (i = c - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
